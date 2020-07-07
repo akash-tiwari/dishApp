c0a52527a7395472e2e45290140dc5b7505e2195
akash-tiwari <takash800@gmail.com>
2020-07-08 00:42:42 +0530

    changed id to dishId

--- routes/dishRouter.js
+++ routes/dishRouter.js
@@ -25,24 +25,24 @@ dishRouter.route("/")
     res.end("will delete dish data");
 });
 
-dishRouter.route("/:id")
+dishRouter.route("/:dishId")
 .all((req, res, next)=>{
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
     next();
 })
 .get((req, res, next)=>{
-    res.end("will send dish data with id = " + req.params.id);
+    res.end("will send dish data with dishId = " + req.params.dishId);
 })
 .post((req, res, next)=>{
     res.statusCode = 403;
     res.end("this operation not allowed.");
 })
 .put((req, res, next)=>{
-    res.end("will change dish data at id = " + req.params.id);
+    res.end("will change dish data at dishId = " + req.params.dishId);
 })
 .delete((req, res, next)=>{
-    res.end("will delete dish data at id = "+ req.params.id);
+    res.end("will delete dish data at dishId = "+ req.params.dishId);
 });
 
 module.exports = dishRouter;
